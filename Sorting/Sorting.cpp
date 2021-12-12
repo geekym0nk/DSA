4bc44cbb73047b135edc69418b2378372cc7b205
Pallavi Bhat <87034919+geekym0nk@users.noreply.github.com>
2021-12-12 08:37:24 +0530

    Create Sorting.cpp

---
+++ Sorting/Sorting.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+using namespace std;
+
+class Sorting {
+    public :
+        void selectionSort(vector<int>& nums)
+        {  
+            for (int i = 0; i < nums.size()-1; i++)
+            {
+                for (int j = i+1; j < nums.size(); j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        nums[i] ^= nums[j];
+                        nums[j] ^= nums[i];
+                        nums[i] ^= nums[j];
+                    }
+                }
+            }
+            
+            return;
+        }
+};
