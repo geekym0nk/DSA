f2821c8f141a6ad3868ea6f3a8e12a5baf9896ba
Pallavi Bhat <87034919+geekym0nk@users.noreply.github.com>
2021-12-12 08:56:29 +0530

    Update Sorting.cpp

--- Sorting/Sorting.cpp
+++ Sorting/Sorting.cpp
@@ -21,4 +21,32 @@ class Sorting {
             
             return;
         }
+        
+        void bubbleSort (vector<int> & nums)
+        {
+            bool isSorted = true;
+            
+            for (int i = nums.size(); i > 0; i--)
+            {
+                for (int j = 0; j < i-1; j++)
+                {
+                    if (nums[j] > nums[j+1])
+                    {
+                        nums[j+1] ^= nums[j];
+                        nums[j] ^= nums[j+1];
+                        nums[j+1] ^= nums[j];
+                        
+                        isSorted = false;
+                    }
+                }
+                
+                if (isSorted)
+                    break;
+                    
+                // Lets assume now it must have been sorted & wait to be proven wrong
+                isSorted = true;
+            }
+            
+            return;
+        }
 };
