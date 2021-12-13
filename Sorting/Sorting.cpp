e6045f40ba15b8934db957a9105564f8806cb7d3
Pallavi Bhat <87034919+geekym0nk@users.noreply.github.com>
2021-12-13 16:24:16 +0530

    Update Sorting.cpp

--- Sorting/Sorting.cpp
+++ Sorting/Sorting.cpp
@@ -45,8 +45,83 @@ class Sorting {
                     
                 // Lets assume now it must have been sorted & wait to be proven wrong
                 isSorted = true;
+            } 
+            return;
+        }
+        
+        void merge (vector<int> & nums, int &lo, int &mid, int &hi)
+        {
+            int itr1 = 0;
+            int itr2 = 0;
+            
+            vector<int> nums1;
+            vector<int> nums2;
+            
+            for (itr1 = lo; itr1 <= mid; itr1++)
+                nums1.push_back(nums[itr1]);
+                
+            for (itr2 = mid+1; itr2 <= hi; itr2++)
+                nums2.push_back(nums[itr2]);
+        
+            itr1 = 0;
+            itr2 = 0;
+            int i = 0;
+            
+            for (i = lo; i <= hi; i++)
+            {
+                if ((itr1 < nums1.size()) &&
+                    (itr2 < nums2.size()))
+                {
+                    if (nums1[itr1] > nums2[itr2])
+                    {
+                        nums[i] = nums2[itr2];
+                        itr2++;
+                    }
+                    else
+                    {
+                        nums[i] = nums1[itr1];
+                        itr1++;
+                    }
+                }
+                else
+                {
+                    if (itr1 < nums1.size())
+                    {
+                        nums[i] = nums1[itr1];
+                        itr1++;
+                    }
+                    else if (itr2 < nums2.size())
+                    {
+                        nums[i] = nums2[itr2];
+                        itr2++;
+                    }
+                }
+            }
+        
+            return;
+        }
+        
+        void mergeSort (vector<int> & nums, int lo, int hi)
+        {
+            if (lo < hi)
+            {
+                int mid = lo + ((hi-lo)/2);
+                
+                mergeSort(nums, lo, mid);
+                mergeSort(nums, mid+1, hi);
+                merge(nums, lo, mid, hi);
             }
             
+            return;
+        }
+        
+        void mergeSort (vector<int> & nums)
+        {
+            if (nums.size() <= 1)
+                return;
+            
+            mergeSort(nums, 0, nums.size()-1);
+
             return;
         }
 };
